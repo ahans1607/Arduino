406b3b4555385224cc7316c56f6760b45c049ad2
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-10-06 23:47:31 -0500

    Update readVoltage.cpp

--- readVoltage.cpp
+++ readVoltage.cpp
@@ -7,7 +7,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(analogPin, INPUT);
   Serial.begin(9600);
-}
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -15,4 +15,4 @@ void loop() {
   V2=((5./1023.) * readVal);
   Serial.println(V2);
   delay(second);
-}
+};
