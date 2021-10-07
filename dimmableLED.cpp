5ec4aeb951fe8c63b12eeb4f1bd2fcd5bd18339b
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-10-07 16:26:43 -0500

    Update dimmableLED.cpp

--- dimmableLED.cpp
+++ dimmableLED.cpp
@@ -1,25 +1,51 @@
-int potPin=A0;
-int ledPin=6;
-int potVal;
-float ledVal;
+// int potPin=A0;
+// int ledPin=6;
+// int potVal;
+// float ledVal;
 
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   pinMode(potPin, INPUT);
+//   pinMode(ledPin, OUTPUT);
+//   Serial.begin(9600);
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   potVal=analogRead(potPin);
+//   ledVal=((255./1023.)*potVal);
+
+//   analogWrite(ledPin, ledVal);
+  
+//   Serial.println(ledVal);
+//   exit(0);
+
+// }
+
+
+
+int analogInput=A0;
+int second=1000;
+float voltageOutput;
+int analogVal;
+int whiteLED=6;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(potPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  
+  pinMode(analogInput, INPUT);
+  pinMode(whiteLED, OUTPUT);
+
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  potVal=analogRead(potPin);
-  ledVal=((255./1023.)*potVal);
-
-  analogWrite(ledPin, ledVal);
+  analogVal=analogRead(analogInput);
   
-  Serial.println(ledVal);
-  exit(0);
+  voltageOutput=((5./1023.)*analogVal);
+  Serial.println(voltageOutput);
 
+  analogWrite(whiteLED, (255./1023.)*analogVal);
 }
