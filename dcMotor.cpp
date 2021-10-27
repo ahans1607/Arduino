a9b39d4d6673c22773ca7f5c953e014b93a8535e
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-10-26 22:49:54 -0500

    Create dcMotor.cpp

---
+++ dcMotor.cpp
@@ -0,0 +1,20 @@
+int speedPin=5;
+int dirPin1=4;
+int dirPin2=3;
+int mSpeed=255;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(speedPin, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+  Serial.begin(2000000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(dirPin1, LOW);
+  digitalWrite(dirPin2, HIGH);
+  analogWrite(speedPin, mSpeed);
+
+}
