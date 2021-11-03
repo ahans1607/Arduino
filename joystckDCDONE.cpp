f5c15f1ca144fbf512845232de0edf5d0a8ff1d2
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-11-02 19:11:56 -0500

    Create joystckDCDONE.cpp

---
+++ joystckDCDONE.cpp
@@ -0,0 +1,57 @@
+int VRxPin = A0;
+int VRyPin = A1;
+int SwPin = 2;
+int SpinVal;
+int xAxVal;
+int yAxVal;
+
+int speedPin=A2;
+int dir1=8;
+int dir2=9;
+
+float speedVal;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(2000000);
+  
+  pinMode(VRxPin, INPUT);
+  pinMode(VRyPin, INPUT);
+  digitalWrite(SwPin, HIGH);
+  pinMode(SwPin, INPUT);
+
+  pinMode(speedPin, OUTPUT);
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  xAxVal=analogRead(VRxPin);
+  yAxVal=analogRead(VRyPin);
+  SpinVal=digitalRead(SwPin);
+
+//  speedVal=((-255./1023.)*yAxVal + 255);
+
+  if(yAxVal < 512){
+    digitalWrite(dir1, HIGH);
+    digitalWrite(dir2, LOW);
+    speedVal=((-255./512.)*yAxVal + 255);
+    analogWrite(speedPin, speedVal);
+    }
+
+   if(yAxVal >= 512){
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, HIGH);
+    speedVal=((255./512.)*yAxVal - 255);
+    analogWrite(speedPin, speedVal);
+    }
+  
+  
+
+//  Serial.println(yAxVal);
+//  Serial.println(speedVal);
+
+}
