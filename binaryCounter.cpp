d45b7ccea0f94bdc4947ad7bb2e7c07a318284d5
Ananya Hans <77026617+ahans1607@users.noreply.github.com>
2021-11-20 01:27:42 -0600

    Create binaryCounter.cpp

---
+++ binaryCounter.cpp
@@ -0,0 +1,27 @@
+int latchPin=11;
+int clockPin=9;
+int dataPin=12;
+
+byte LED=0b00000000;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED);
+  digitalWrite(latchPin, HIGH);
+  LED+=1;
+  
+
+  delay(100);
+  
+
+}
